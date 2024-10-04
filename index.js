6b5bd7894b8b26f738283401634918ebeb48519e
Shashank127Yadav <Shashank127Yadav@gmail.com>
2024-10-04 19:41:03 +0530

    server-side-rendering

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url");
 const URL = require("./models/url");
@@ -10,12 +11,20 @@ connectToMongoDB("mongodb://localhost:27017/url-shortner").then(() =>
   console.log("Mongodb Connected")
 );
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 // Middleware
 app.use(express.json());
 
+app.get("/test", async (req, res) => {
+  const allUrls = await URL.find({});
+  return res.render("home");
+});
+
 app.use("/url", urlRoute);
 
-app.get("/:shortId", async (req, res) => {
+app.get("url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     {
